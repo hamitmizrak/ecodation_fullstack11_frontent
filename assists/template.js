bb7d1352ee145cde988fe38bea8295080c6eecba
Hamit Mızrak <hamitmizrak@gmail.com>
2023-08-20 09:41:55 +0300

    Js

---
+++ assists/template.js
@@ -0,0 +1,26 @@
+// Kod açaıklamalarını(single comment)
+/* Çoklu açıklamalar ()Multiple comment*/
+
+// window.alert("pop-up");
+// document.writeln("Sayfada göster");
+console.log("console log");
+console.error("console error");
+console.warn("console warn");
+
+// variable
+// var  let(ES6)  const(ES6) 
+var degiskenAdi=44;
+console.log(degiskenAdi);
+console.log( typeof degiskenAdi);
+
+var $_degiskenAdi44="str variable";
+console.log($_degiskenAdi44);
+console.log( typeof $_degiskenAdi44);
+
+// hoisting
+deneme=14.53;
+var deneme;
+console.log(deneme);
+
+////////////////////////////////////////////////$_degiskenAdi44
+// Operators
