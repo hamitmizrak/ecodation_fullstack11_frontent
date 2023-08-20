806b30d34399c07ec97bbd31eed909318ccc796c
Hamit Mızrak <hamitmizrak@gmail.com>
2023-08-20 09:57:04 +0300

    Operators,  prompt

--- assists/template.js
+++ assists/template.js
@@ -3,24 +3,45 @@
 
 // window.alert("pop-up");
 // document.writeln("Sayfada göster");
-console.log("console log");
-console.error("console error");
-console.warn("console warn");
+// console.log("console log");
+// console.error("console error");
+// console.warn("console warn");
 
 // variable
 // var  let(ES6)  const(ES6) 
-var degiskenAdi=44;
-console.log(degiskenAdi);
-console.log( typeof degiskenAdi);
+// var degiskenAdi=44;
+// console.log(degiskenAdi);
+// console.log( typeof degiskenAdi);
 
-var $_degiskenAdi44="str variable";
-console.log($_degiskenAdi44);
-console.log( typeof $_degiskenAdi44);
+// var $_degiskenAdi44="str variable";
+// console.log($_degiskenAdi44);
+// console.log( typeof $_degiskenAdi44);
 
 // hoisting
-deneme=14.53;
-var deneme;
-console.log(deneme);
+// deneme=14.53;
+// var deneme;
+// console.log(deneme);
 
-////////////////////////////////////////////////$_degiskenAdi44
+////////////////////////////////////////////////
 // Operators
+// + - * / %
+let number1,number2;
+number1=18;
+number2=5;
+console.log(number1+number2);
+console.log(number1*number2);
+console.log(number1-number2);
+console.log(number1/number2);
+console.log(number1%number2);
+
+// &&VE ||VEYA     !DEĞİL
+// x++ ++x  y-- --y
+
+// =   ATAMA
+// ==  EŞİTTİR(Türüne bakmadan)
+// === EŞİTTİR(Türüne bakarak)
+
+////////////////////////////////////////////////
+// prompt
+let userName=prompt("Lütfen birşeyler yazınız");
+console.log(userName);
