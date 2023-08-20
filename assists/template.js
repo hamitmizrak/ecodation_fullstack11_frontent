d7c1e8e6033c9fe3c32a75a9c8d123ef53c5ad4a
Hamit Mızrak <hamitmizrak@gmail.com>
2023-08-20 10:13:11 +0300

    prompt

--- assists/template.js
+++ assists/template.js
@@ -25,14 +25,14 @@
 ////////////////////////////////////////////////
 // Operators
 // + - * / %
-let number1,number2;
-number1=18;
-number2=5;
-console.log(number1+number2);
-console.log(number1*number2);
-console.log(number1-number2);
-console.log(number1/number2);
-console.log(number1%number2);
+// let number1,number2;
+// number1=18;
+// number2=5;
+// console.log(number1+number2 );
+// console.log(number1*number2);
+// console.log(number1-number2);
+// console.log(number1/number2);
+// console.log(number1%number2);
 
 // &&VE ||VEYA     !DEĞİL
 // x++ ++x  y-- --y
@@ -43,5 +43,44 @@ console.log(number1%number2);
 
 ////////////////////////////////////////////////
 // prompt
-let userName=prompt("Lütfen birşeyler yazınız");
-console.log(userName);
+// let userName=prompt("Lütfen birşeyler yazınız");
+// console.log(userName);
+
+
+//////////////////////////////////////////////// 
+// NaN
+console.log(4/4);
+console.log(4/"4");
+console.log(4/"asd");
+
+
+// Infinity
+console.log(0/5);
+console.log(44/0);
+
+// Undefined
+let data;
+//data=99;
+console.log(data);
+
+// new 
+// null 
+// zero
+
+
+//////////////////////////////////////////////// 
+// Örnek-1
+// Kullanıcıdan aldığımız 2 sayıyı toplayan JS kodu yazınız ? (prompt)
+
+// Örnek-2
+/* 
+    Kullanıcıdan aldığımız sayıyı dereceden fahrenhayt'a çeviren JS kodu yazınız ?
+    Formül: (derece*9/5)+32   
+*/
+
+// Örnek-3
+// y=3x+4k ==>1.dereceden 2 bilinmeyenli denklem algoritma JS kodu yazınız ?
+// Kullanıcı tarafından alınan x ve kdeğerlerini hesaplayan algoritma yazınız ?
+
+// örnek-4 operatör işlemleri: aşağıdaki örneği javascript ile yapalım
+// 4+3*2(3:3-1*6+9:1+(3:3))
